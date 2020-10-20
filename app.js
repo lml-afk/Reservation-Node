7a137b2fddeebe512f22aec3df90540cca788134
lml-afk <la.hi@hotmail.com>
2020-10-20 23:36:11 +0300

    delete

--- app.js
+++ app.js
@@ -4,19 +4,30 @@ const mongoose = require('mongoose');
 const app = express();
 require('dotenv').config();
 const {MongoClient} = require('mongodb');
+const url = require('url');
 const PORT = process.env.PORT || 8080;
-var merge = require('merge');
 
+//get mongoose url from .env file
 const mongoose_url = process.env.mongoose_url;
 
 app.use(body_parser.json());
-
 app.use(body_parser.urlencoded({
     extended: true
 }));
 
 const user_model = require('./models/user_model');
 const reservation_model = require('./models/reservation_model');
+
+app.get('/reservations/register', function(req,res){
+    const html = '<form action="/reservations/register" method="POST">'+
+    'Enter username:' + 
+    '<input type="text" name="user_name" id="user_name"/>'+
+    "Enter password: " +
+    '<input type="text" name="user_password" id="user_password"/>' +
+    '<br>' +
+    '<button type="submit">submit</button>\n</form>'
+    res.send(html);
+});
 
 app.post('/reservations/register',function (req, res) {
     const user_name = req.body.user_name;
@@ -32,7 +43,6 @@ app.post('/reservations/register',function (req, res) {
             res.json('User already exists');
         }
         let new_user = new user_model({
-            //set name to lower case for easier queries later
             name: user_name.toLowerCase(),
             password: user_password,
             reservations: []
@@ -44,6 +54,19 @@ app.post('/reservations/register',function (req, res) {
     });
 });
 
+app.get('/reservations/reservation',function (req,res) {
+    const html = '<form action="/reservations/reservation" method="POST">'+
+    'Enter username:' + 
+    '<input type="text" name="user_name" id="user_name"/>'+
+    "Enter event name: " +
+    '<select name="eventname" id="eventname"><option value="poraus">Poraus</option><option value="paikkaus">Paikkaus</option><option value="tarkistus">Tarkistus</option> <option value="tekohampaat">Tekohampaat</option></select>'+
+    "Choose date and time: "+
+    '<input type="datetime-local" name="start_time" id="start_time" placeholder="select starting time and date"/>' +
+    '<br>' +
+    '<button type="submit">submit</button>\n</form>'
+    res.send(html);
+
+})
 
 app.post('/reservations/reservation',function (req, res){
 
@@ -105,6 +128,8 @@ app.get('/reservations/all_reservations', async function(req,res){
       }); 
 })
 
+
+
 app.patch('/reservations/update:id', function(req,res){
 
     const id = req.body.reservation_id;
@@ -112,9 +137,10 @@ app.patch('/reservations/update:id', function(req,res){
 
     const end_time = new Date(time_to_change);
     end_time.setHours(end_time.getHours() +1);
-    
+
     const merged =[];
 
+        //show original reservation
     reservation_model.findById(id, function (err, original){
         if(err){
             res.send(err);
@@ -138,6 +164,53 @@ app.patch('/reservations/update:id', function(req,res){
             }}
       );
     });
+
+    // http://localhost:8080/reservations/user_reservations/?id=5f8f166aa93e373460808cca
+
+app.get('/reservations/user_reservations', function(req,res){
+
+    const queryObject = url.parse(req.url,true).query
+    console.log(queryObject)
+
+    if(queryObject.id){
+
+    id = queryObject.id;
+        user_model.findById(id).then((user) =>{
+        user.populate('reservations')
+        .execPopulate()
+        .then(()=> {
+            let data = user.reservations
+            let html = 'Current user:' +user.name+ '<hr>'
+            data.forEach((reservation)=>{
+                html+= 'procedure: '+reservation.eventname;
+                html+= '<br>'
+                html+= 'start time: '+reservation.start_time;
+                html+= '<br>'
+                html+= 'end time: '+reservation.end_time;
+                html+= '<br>'
+                html+= 'id: '+reservation._id;
+                html+= '<hr>'
+                })
+            res.send(html)
+            })
+        })
+        }
+        else{
+            res.send('Incorrect id')
+        }
+})
+
+app.post('/reservations/deleteres:id', function (req,res){
+
+    const queryObject = url.parse(req.url,true).query
+    console.log(queryObject)
+
+    if(queryObject.id){
+
+    id = queryObject.id;
+        reservation_model.findByIdAndDelete(id).then((user) =>{
+})
+
 
 mongoose.connect(mongoose_url, {
     useUnifiedTopology: true,
